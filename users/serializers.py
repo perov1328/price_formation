8396c5142640844605722dbd65019e76a151c839
Alexander Perov <perov97@yandex.ru>
2024-02-16 17:46:57 +0300

    password hashing when creating and editing an Account model

--- users/serializers.py
+++ users/serializers.py
@@ -6,6 +6,21 @@ class UserSerializer(serializers.ModelSerializer):
     """
     Сериализатор для модели Пользователя
     """
+    password = serializers.CharField(write_only=True, required=False)
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        if password:
+            instance.set_password(password)
+        return super().update(instance, validated_data)
+
     class Meta:
         model = User
         fields = ('id', 'email', 'phone', 'city', 'first_name', 'last_name', 'password',)
